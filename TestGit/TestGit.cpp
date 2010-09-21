47cda22efd80f34f63cb40b972091322691a8888
Nikolay Kobzarev <kolankob@gmail.com>
2010-09-21 21:54:53 +0400

    initial project state

---
+++ TestGit/TestGit.cpp
@@ -0,0 +1,13 @@
+// TestGit.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Hello World!"<<std::endl;
+	return 0;
+}
+
